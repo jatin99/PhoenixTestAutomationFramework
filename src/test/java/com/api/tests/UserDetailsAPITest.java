2a0c353ac5ad8f914daf3064c4817235a97bbee4
Jatin V Sharma <jatinvsharma@gmail.com>
2025-09-19 16:46:54 +0530

    Requirement 3:
    Created AuthTokenProvider- Manage the Token for Different Roles.
    
    Role: Hold all the User Roles of the Phoneix Application
    
    UserDetailsAPITest: Functional API Test checks the status code, response
    time and Schema of the UserDetails API
    
    CountAPITest:Functional API Test checks the status code, response time
    and Schema of the Count API. Also does the token testing for negative
    scenarios
    
    MasterAPI: Functional API Test checks the status code, response time and Schema of the Master API. Also does the token testing for negative scenarios

--- src/test/java/com/api/tests/UserDetailsAPITest.java
+++ src/test/java/com/api/tests/UserDetailsAPITest.java
@@ -7,6 +7,10 @@ import java.io.IOException;
 
 import org.testng.annotations.Test;
 
+import static com.api.constant.Role.*;
+
+import static com.api.utils.AuthTokenProvider.*;
+
 import static com.api.utils.ConfigManager.*;
 
 import io.restassured.http.ContentType;
@@ -21,7 +25,7 @@ public class UserDetailsAPITest {
 	
 		
 		
-		Header authHeader = new Header("Authorization", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6NCwiZmlyc3RfbmFtZSI6ImZkIiwibGFzdF9uYW1lIjoiZmQiLCJsb2dpbl9pZCI6ImlhbWZkIiwibW9iaWxlX251bWJlciI6Ijg4OTk3NzY2NTUiLCJlbWFpbF9pZCI6Im1hcmtAZ21haWwuY29tIiwicGFzc3dvcmQiOiI1ZjRkY2MzYjVhYTc2NWQ2MWQ4MzI3ZGViODgyY2Y5OSIsInJlc2V0X3Bhc3N3b3JkX2RhdGUiOm51bGwsImxvY2tfc3RhdHVzIjowLCJpc19hY3RpdmUiOjEsIm1zdF9yb2xlX2lkIjo1LCJtc3Rfc2VydmljZV9sb2NhdGlvbl9pZCI6MSwiY3JlYXRlZF9hdCI6IjIwMjEtMTEtMDNUMDg6MDY6MjMuMDAwWiIsIm1vZGlmaWVkX2F0IjoiMjAyMS0xMS0wM1QwODowNjoyMy4wMDBaIiwicm9sZV9uYW1lIjoiRnJvbnREZXNrIiwic2VydmljZV9sb2NhdGlvbiI6IlNlcnZpY2UgQ2VudGVyIEEiLCJpYXQiOjE3NTc1MTA5OTR9.ru3esatir9jfYp4g9pUlMXgeIjxU98gP5liVuVWl6PA");
+		Header authHeader = new Header("Authorization", getToken(SUP));
 		given()
 		.baseUri(getProperty("BASE_URI"))
 		.and()
