68772070241561be25bae9d080d5e0315d5a5f01
Jatin V Sharma <jatinvsharma@gmail.com>
2025-10-29 17:35:31 +0530

    Created the DataProvider Util for the Excel Data that is used in the Createjob api test

--- src/test/java/com/dataproviders/DataProviderUtils.java
+++ src/test/java/com/dataproviders/DataProviderUtils.java
@@ -63,4 +63,22 @@ public class DataProviderUtils {
 		return ExcelReaderUtil2.loadTestData("testData/PhoenixTestData.xlsx", "LoginTestData", UserCredentials.class);
 	}
 
+	@DataProvider(name = "CreateJobAPIExcelDataProvider", parallel = true)
+	public static Iterator<CreateJobPayload> CreateJobAPIExcelDataProvider() {
+		Iterator<CreateJobBean> iterator = ExcelReaderUtil2.loadTestData("testData/PhoenixTestData.xlsx",
+				"CreateJobTestData", CreateJobBean.class);
+
+		List<CreateJobPayload> payloadList = new ArrayList<CreateJobPayload>();
+		CreateJobBean tempBean;
+		CreateJobPayload tempPayload;
+		while (iterator.hasNext()) {
+			tempBean = iterator.next();
+			tempPayload = CreateJobBeanMapper.mapper(tempBean);
+			payloadList.add(tempPayload);
+		}
+
+		return payloadList.iterator();
+
+	}
+
 }
